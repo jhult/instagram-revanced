abd3ac99c94cd06ce7679bfb381c8e8e532cf086
Jonathan Hult <Jonathan@JonathanHult.com>
2025-12-18 14:08:53 -0600

    Force Instagram to use latest version
    Sets lock_version=1 to bypass ReVanced patches version constraints and always download the latest Instagram version.
    
    The ReVanced patches were constraining to version 401.0.0.48.79 due to the "Hide navigation buttons" patch, but since we don't
    use that patch, all our included patches work with any version.

--- src/build/Revanced.sh
+++ src/build/Revanced.sh
@@ -5,5 +5,7 @@ source ./src/build/utils.sh
 dl_gh "revanced-patches revanced-cli" "revanced" "latest"
 
 get_patches_key "instagram"
+# Force using latest version from APKPure instead of constrained version from patches
+lock_version=1
 get_apkpure "com.instagram.android" "instagram-arm64-v8a" "instagram-android/com.instagram.android" "Bundle"
 patch "instagram-arm64-v8a" "revanced"
